000abdfd7fbee183b7303229ff23ed414f4d59b7
Rafac <5720100736@utrng.edu.mx>
2021-05-28 19:56:32 -0500

    el setup esta listo

---
+++ IdentityManager/Data/ClaimStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Data
+{
+    public static class ClaimStore
+    {
+        public static List<Claim> claimsList = new List<Claim>()
+        {
+            new Claim("Crear","Create"),
+            new Claim("Editar","Edit"),
+            new Claim("Eliminar","Delete")
+        };
+    }
+}
